c6d57c9626a75be7e1a6d8d010c36efe81578fd3
Teerth Patel <teerth1205@gmail.com>
2020-02-04 20:02:41 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,8 @@
 class Artist 
-  attr_accessor :name
+  attr_accessor :artist :name
   
-  def initialize(name)
+  def initialize(artist, name)
+    @artist = artist
     @name  = name
   end 
 end 
